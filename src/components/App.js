4514cd6edc8aa7f5c1f487507b9f105d23dd6aba
Aakash Tyagi <aakash.tyagi@codingninjas.in>
2020-03-15 10:37:19 +0530

    Adding store and dispatching actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
-import { data } from '../data';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
-        </div>
+class App extends React.Component {
+  render() {
+    const data = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
 
-        <div id="list">
-          {data.map(movie => (
-            <MovieCard movie={movie} />
-          ))}
+          <div id="list">
+            {data.map(movie => (
+              <MovieCard movie={movie} key={movie.imdbID} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import movies from './reducers/movies';
+import { data as moviesList } from './data';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(movies);
+// console.log(store);
+console.log('state', store.getState());
+
+// update store by dispatching actions
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: moviesList
+});
+console.log('state', store.getState());
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
