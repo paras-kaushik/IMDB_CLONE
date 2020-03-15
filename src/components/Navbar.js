f8bc2d3ee41184423a4952383e1963252b2d5547
Aakash Tyagi <aakash.tyagi@codingninjas.in>
2020-03-15 13:32:50 +0530

    Add search bar UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import { data } from '../data';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSearchResults: true
+    };
+  }
+  handleAddToMovies = () => {
+    // mini assignment
+  };
+
+  handleSearch = () => {
+    // next session
+  };
+
   render() {
+    const { showSearchResults } = this.state;
     return (
       <div className="nav">
         <div className="search-container">
           <input />
-          <button>Search</button>
+          <button id="search-btn" onClick={this.handleSearch}>
+            Search
+          </button>
+
+          {showSearchResults && (
+            <div className="search-results">
+              <div className="search-result">
+                <img src={data[0].Poster} alt="search-pic" />
+                <div className="movie-info">
+                  <span>{data[0].Title}</span>
+                  <button onClick={this.handleAddToMovies}>
+                    Add to Movies
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     );
