479d66b78b4b23cace337984fecb7769bfdf5425
Aakash Tyagi <aakash.tyagi@codingninjas.in>
2020-03-15 14:19:06 +0530

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 import rootReducer from './reducers';
-import { data as moviesList } from './data';
 import './index.css';
 
-const store = createStore(rootReducer);
+// const logger = function({ dispatch, getState }) {
+//   return function(next) {
+//     return function(action) {
+//       // my middlware
+//       console.log('ACTION', action);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger = ({ dispatch, getState }) => next => action => {
+  // my middlware
+  console.log('ACTION', action);
+  next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 // console.log(store);
 console.log('state', store.getState());
 
