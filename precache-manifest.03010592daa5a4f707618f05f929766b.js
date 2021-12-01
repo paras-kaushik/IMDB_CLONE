7bc6517b3d6fa413d54ef233961e3bafb38da10e
Paras Kaushik <paraskaushik12@gmail.com>
2021-12-01 13:03:40 +0530

    Updates

---
+++ precache-manifest.03010592daa5a4f707618f05f929766b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9703edc3c5d1c782e6834219a18247",
+    "url": "/IMDB_CLONE/index.html"
+  },
+  {
+    "revision": "d9e3b8f028ee31d133b7",
+    "url": "/IMDB_CLONE/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "15a83242727e6cbe893c",
+    "url": "/IMDB_CLONE/static/js/2.0b1752df.chunk.js"
+  },
+  {
+    "revision": "05910089a23dbf57a02bd2eaff51ace8",
+    "url": "/IMDB_CLONE/static/js/2.0b1752df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e3b8f028ee31d133b7",
+    "url": "/IMDB_CLONE/static/js/main.92a22e3f.chunk.js"
+  },
+  {
+    "revision": "8a411a8bf81fbb3b9a2b",
+    "url": "/IMDB_CLONE/static/js/runtime-main.b3a21169.js"
+  }
+]);
